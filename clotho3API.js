3cf6ee09ebd4e9372ad485d775907cad1ceb5eea
Kevin LeShane <Kevin@Kevins-MacBook-Pro.local>
2013-12-26 13:51:18 -0500

    Added Clotho client method prototypes

--- clotho3API.js
+++ clotho3API.js
@@ -61,63 +61,133 @@ socket.onmesssage = function(evt) {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 											Objects and Methods											 //
+// 												 Methods											     //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+* @name clientAPI.create
+* @param objects: set of one or more JSON objects describing an instance(s) of Clotho schema.
+* @description Creates object(s) as defined by the input parameter. 
+* @return An ID or list of IDs created.
+*/
+var create = function(objects) {
+	return objectIDs;
+}
+
+/**
+* @name clientAPI.destroy
+* @param objects: One or more JSON object(s) describing an instance(s) of Clotho schema.
+* @description Destroys object(s) as defined by the input.
+*/
+var destroy = function(objects) {
+	//nothing to return
+}
+
+/**
+* @name clientAPI.set
+* @param objectSpecs: A JSON object specification with the ID field(s) of the Clotho object instance to be altered. 
+* @description Sets the fields present in the specificiation to the values defined by the spec. 
+* @return An ID or list of IDs of objects updated.
+*/
+var set = function(objectSpecs){
+	return objectIDs;
+}
+
+/**
+* @name clientAPI.get
+* @param objectSelectors: JSON object selector(s) describing an instance(s) of Clotho schema.
+* @description Gets object(s) as defined by the input parameter(s). 
+* @return Object description for every input object requested.
+*/
+var get = function(objectSelectors) {
+	return objects;
+}
+
+/**
+* @name clientAPI.query
+* @param objectSpec: Clotho object specification.
+* @description Seeks all Clotho object instances that match the object specification. 
+* @return All objects that match the spec.
+*/
+var query = function(objectSpec) {
+	return objects;
+}
+
+/**
+* @name clientAPI.queryAll
+* @param objectSpec: Clotho object specification.
+* @description Seeks Clotho object instances that match the object specification. 
+* @return The first Clotho object that matches the spec.
+*/
+var queryOne = function(objectSpec) {
+	return object;
+}
+
+/**
+* @name clientAPI.run
+* @param function: object selector indicating function to run.
+		 args: JSON object with key-value pairs providing the argument values to the function.
+* @description Executes specified function with args as its input.
+*/
+var run = function(function, args) {
+	//nothing to return
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // BASIC_PART
-function BasicPart(name, description, seq, format, author) {	// Command server to create new Basic Part
-	send("create", '{"schema":"BasicPart", "name":'+name+', "description":'+description+', "seq":'+seq+', "format":'+format+', "author":'+author+'}', ... );
-}
-function getBasicPart(name) {	// Query server to get existing Basic Part.
-	// Should I not specify "BasicPart" schema here? Should I query for uuid in place of name?
-	send("query", '{"schema":"BasicPart", "name":'+name+'}', function(data) {
-		return data;
-	});	
-}
-BasicPart.prototype = {
-	getId: function() { return this.id; };	// Returns id of part	
-	getName: function() { return this.name; };	// Returns name of part
-	getAuthorId: function() { return this.author; }; 	// Returns author id associated with this part
-	getDescription: function() { return this.shortDescription; };	// Returns description of this part
-	getFormat: function() { return this.form };	// Returns Part Format object
-}
+// function BasicPart(name, description, seq, format, author) {	// Command server to create new Basic Part
+// 	send("create", '{"schema":"BasicPart", "name":'+name+', "description":'+description+', "seq":'+seq+', "format":'+format+', "author":'+author+'}', ... );
+// }
+// function getBasicPart(name) {	// Query server to get existing Basic Part.
+// 	// Should I not specify "BasicPart" schema here? Should I query for uuid in place of name?
+// 	send("query", '{"schema":"BasicPart", "name":'+name+'}', function(data) {
+// 		return data;
+// 	});	
+// }
+// BasicPart.prototype = {
+// 	getId: function() { return this.id; };	// Returns id of part	
+// 	getName: function() { return this.name; };	// Returns name of part
+// 	getAuthorId: function() { return this.author; }; 	// Returns author id associated with this part
+// 	getDescription: function() { return this.shortDescription; };	// Returns description of this part
+// 	getFormat: function() { return this.form };	// Returns Part Format object
+// }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// COMPOSITE_PART
-function CompositePart() {	// Command server to create new Composite Part
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // COMPOSITE_PART
+// function CompositePart() {	// Command server to create new Composite Part
 
-}
-function getCompositePart(name) {	// Query server to get existing Composite Part
-	send("query", '{"schema":"CompositePart", "name":'+name+'}', function(data) {
-		return data;
-	});
-}
-CompositePart.prototype = {
+// }
+// function getCompositePart(name) {	// Query server to get existing Composite Part
+// 	send("query", '{"schema":"CompositePart", "name":'+name+'}', function(data) {
+// 		return data;
+// 	});
+// }
+// CompositePart.prototype = {
 
-}
+// }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// PLASMID
-function Plasmid(part, vector, author, format) {	// Plasmid object constructor
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // PLASMID
+// function Plasmid(part, vector, author, format) {	// Plasmid object constructor
 	
-};
-function getPlasmid(name) {	// Query server to get existing plasmid using entered specifics
-	send("query", '{"schema":"Plasmid", "name":'+name+'}', function(data) {
-		return data;
-	});
-};
-Plasmid.prototype = {
-	getVectorId: function() {  };	// Returns id of vector object associated with this plasmid.
-	getPartId: function() {  };	// Returns id of part object associated with this plasmid.		
-	getAuthorId: function() {  };	// Returns id of author object associated with this plasmid.
-	getFormatId: function() {  };	// Returns id of format table object associated with this plasmid.
-	getName: function() {  };	// Returns plasmid name
-	getConstructionFile: function() {  };	// Returns plasmid's construction file
-	getUUID: function() {  };	// Returns plasmid's UUID
-	getDateCreated: function() {  };	// Returns date plasmid was created
-	getLastModified: function() {  };	// Returns date plasmid was last modified
-};
+// };
+// function getPlasmid(name) {	// Query server to get existing plasmid using entered specifics
+// 	send("query", '{"schema":"Plasmid", "name":'+name+'}', function(data) {
+// 		return data;
+// 	});
+// };
+// Plasmid.prototype = {
+// 	getVectorId: function() {  };	// Returns id of vector object associated with this plasmid.
+// 	getPartId: function() {  };	// Returns id of part object associated with this plasmid.		
+// 	getAuthorId: function() {  };	// Returns id of author object associated with this plasmid.
+// 	getFormatId: function() {  };	// Returns id of format table object associated with this plasmid.
+// 	getName: function() {  };	// Returns plasmid name
+// 	getConstructionFile: function() {  };	// Returns plasmid's construction file
+// 	getUUID: function() {  };	// Returns plasmid's UUID
+// 	getDateCreated: function() {  };	// Returns date plasmid was created
+// 	getLastModified: function() {  };	// Returns date plasmid was last modified
+// };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ANNOTATION
