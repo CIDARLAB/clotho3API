0c10353cec2bb5360424bb5e42ddf5b24beb5c24
leshane <leshane@bu.edu>
2013-12-29 00:08:16 -0500

    Small syntax changes

--- clotho3API.js
+++ clotho3API.js
@@ -1,16 +1,19 @@
+// TODO: How to ensure these libraries below are used? Cannot import js libs in other js files.
 // Pull q library hosted at cdnjs
-<script src="//cdnjs.cloudflare.com/ajax/libs/q.js/0.9.6/q.min.js"></script>
+//<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/q.js/0.9.6/q.min.js"></script>
 // Pull Socket.IO from cdn
-<script src="http://cdn.socket.io/stable/socket.io.js"></script>
+//<script type="text/javascript" src="http://cdn.socket.io/stable/socket.io.js"></script>
+
+var Q = require('q');
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 								 Websocket socket and event handlers								 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// var socket = io.connect('http://localhost:8080');
-
-var socket = new WebSocket('ws://localhost:8080/websocket');	//creates new web socket socket
-var callbackHash = {}; // key: request id, value: callback function
+// Creates new web socket socket
+var socket = new WebSocket('ws://localhost:8080/websocket');
+// Key: request id, Value: callback function
+var callbackHash = {};
 var requestID = 0;	
 
 // Websocket socket open
@@ -30,19 +33,32 @@ socket.onerror = function(evt) {
 
 // Client sends message to server
 var send = function(channel, data, callback) {
-    if (socket.readyState === 1) {
-        // Request ID is assigned current time value
-        var requestID = new Date().getTime();
-        // Construct message to send
-        var message = '{"channel":"' + channel + '", "data":' + data + ',"requestId":"' + requestID + '"}';
-        // Hash callback function with its corresponding requestID
-        callbackHash[requestID] = callback;
-        // Send message
-        socket.send(message);
-    } else {
-        // Open new websocket if one is not detected
+    if (socket.readyState !== 1) {
+    	// Open new websocket if one is not detected
         socket = new WebSocket('ws://localhost:8080/websocket');
     }
+    // Request ID is assigned current time value
+    var requestID = new Date().getTime();
+    // Construct message to send
+    var message = '{"channel":"' + channel + '", "data":' + data + ',"requestId":"' + requestID + '"}';
+    // Hash callback function with its corresponding requestID
+    callbackHash[requestID] = callback;
+    // Send message
+    socket.send(message);
+
+    // if (socket.readyState === 1) {
+    //     // Request ID is assigned current time value
+    //     var requestID = new Date().getTime();
+    //     // Construct message to send
+    //     var message = '{"channel":"' + channel + '", "data":' + data + ',"requestId":"' + requestID + '"}';
+    //     // Hash callback function with its corresponding requestID
+    //     callbackHash[requestID] = callback;
+    //     // Send message
+    //     socket.send(message);
+    // } else {
+    //     // Open new websocket if one is not detected
+    //     socket = new WebSocket('ws://localhost:8080/websocket');
+    // }
 };
 
 // Client receives data from the server
@@ -54,7 +70,7 @@ socket.onmesssage = function(evt) {
     var requestId = dataJSON["requestId"];
     if (requestId !== null) {
         // If callback function exists, run it
-        var callback = callbackHash[requestId];
+        var callback = callbackHash[requestId];	
         if (callback !== undefined) {
             callback(dataJSON["data"]);
             delete callbackHash[requestId];
@@ -67,71 +83,71 @@ socket.onmesssage = function(evt) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
-* @name Clotho.create
-* @param A list of one or more JSON objects describing an instance(s) of Clotho schema.
-* @description Creates specified object(s) and associated UUIDs in the order they are found in the list (if more than one). 
-* @return A list of created objects
+* Clotho.create
+* Creates specified object(s) and associated UUIDs in the order they are found in the list (if more than one). 
+* @param {Object} A list of one or more JSON objects describing an instance(s) of Clotho schema. 
+* @return {Object} A list of created objects
 */
 var create = function(objects) {
-	return objectIDs;
+	send('create', objects, createCallback);
+	return createdObjects;
 }
 
 /**
-* @name Clotho.destroy
-* @param objects: One or more JSON object(s) describing an instance(s) of Clotho schema.
-* @description Destroys object(s) as defined by the input.
+* Clotho.destroy
+* Destroys object(s) as defined by the input.
+* @param {Object} One or more JSON object(s) describing an instance(s) of Clotho schema.
 */
 var destroy = function(objects) {
 	//nothing to return
 }
 
 /**
-* @name Clotho.set
-* @param objectSpecs: A JSON object specification with the ID field(s) of the Clotho object instance to be altered. 
-* @description Sets the fields present in the specificiation to the values defined by the spec. 
-* @return An ID or list of IDs of objects updated.
+* Clotho.set
+* Sets the fields present in the specificiation to the values defined by the spec. 
+* @param {Object} A JSON object specification with the ID field(s) of the Clotho object instance to be altered. 
+* @return {Object} An ID or list of IDs of objects updated.
 */
 var set = function(objectSpecs){
 	return objectIDs;
 }
 
 /**
-* @name Clotho.get
-* @param objectSelectors: JSON object selector(s) describing an instance(s) of Clotho schema.
-* @description Gets object(s) as defined by the input parameter(s). 
-* @return Object description for every input object requested.
+* Clotho.get
+* Gets object(s) as defined by the input parameter(s).
+* @param {Object} JSON object selector(s) describing an instance(s) of Clotho schema. 
+* @return {Object} Object description for every input object requested.
 */
 var get = function(objectSelectors) {
 	return objects;
 }
 
 /**
-* @name Clotho.query
-* @param objectSpec: Clotho object specification.
-* @description Seeks all Clotho object instances that match the object specification. 
-* @return All objects that match the spec.
+* Clotho.query
+* Seeks all Clotho object instances that match the object specification. 
+* @param {Object} Clotho object specification.
+* @return {Object} All objects that match the spec.
 */
 var query = function(objectSpec) {
 	return objects;
 }
 
 /**
-* @name Clotho.queryAll
-* @param objectSpec: Clotho object specification.
-* @description Seeks Clotho object instances that match the object specification. 
-* @return The first Clotho object that matches the spec.
+* Clotho.queryAll
+* Seeks Clotho object instances that match the object specification.
+* @param {Object} Clotho object specification. 
+* @return {Object} The first Clotho object that matches the spec.
 */
 var queryOne = function(objectSpec) {
 	return object;
 }
 
 /**
-* @name Clotho.run
-* @param function: object selector indicating function to run.
-		 args: JSON object with key-value pairs providing the argument values to the function.
-* @description Executes specified function with args as its input.
+* Clotho.run
+* Executes specified function with args as its input.
+* @param {Object} Object selector indicating the function to run. Args: JSON object with key-value pairs providing the argument values to the function.
 */
-var run = function(function, args) {
+var run = function(myFunction, args) {
 	//nothing to return
 }
 
