2064af3a8d6689d45dd8a86537a37fc09d552c1d
leshane <leshane@bu.edu>
2014-02-11 02:54:03 -0500

    Simplified testing JS for first workable JUnit test

--- testClothoAPI/clothoAPItests.js
+++ testClothoAPI/clothoAPItests.js
@@ -1,296 +1,81 @@
-
+        
 module("Server API Tests");
 
-var Message = function (channel, data, requestId) {
-    this.channel = channel;
-    this.data = data;
-};
+////////////////////////////////////////////////////////
 
-var getSocket = function (addr) {
+// Creates new web socket socket
+// var socket = new WebSocket('wss://localhost:8443/websocket');
+// Callback function hash table --> Key: request id, Value: callback function
+// var callbackHash = {};
+var requestID = 0;  
+
+// // Websocket socket open
+// socket.onopen = function(evt) {
+//     // TODO: Authenticate websocket socket
+// };
+// //websocket socket closed
+// socket.onclose = function(evt) {
+//     alert("Closing websocket socket");
+// };
+// //websocket error occurs
+// socket.onerror = function(evt) {
+//     alert("Socket error. Is Clotho running?");
+// };
+
+var getSocket = function(addr) {
     socket = new WebSocket(addr);
-    socket.idx = -1;
-    socket.callbacks = {};
-        
+    var callbackHash = {};
+
     socket.oldsend = socket.send;
 
-    socket.send = function (message, callback) {
-        socket.idx += 1;
-        message.requestId = String(socket.idx);
-        socket.callbacks[message.channel+message.requestId] = callback;
-        socket.oldsend(JSON.stringify(message));
+    // Helper function: Client sends message to server
+    socket.send = function(channel, data, callback) {
+        // if (socket.readyState !== 1) {
+        //     // Open new websocket if one is not detected
+        //     socket = new WebSocket('wss://localhost:8443/websocket');
+        // }
+        // Create 'deferred' object ... Q is a global variable created simply by adding the Q library script tag
+        // var deferred = Q.defer();
+        // New Request ID is assigned current time value
+        var requestID = new Date().getTime();
+        // Construct message to send
+        var message = '{"channel":"' + channel + '", "data":"' + data + '","requestId":"' + requestID + '"}';
+        // Hash callback function with its corresponding requestID
+        callbackHash[channel + requestID] = callback;
+        // var socket = new WebSocket('wss://localhost:8443/websocket');
+        // Send message
+        socket.oldsend(message);
+        // Return promise
+        // return deferred.promise;
     };
-    socket.onmessage = function (e) {
-        var message = JSON.parse(e.data);
-        var callbackKey = message.channel+message.requestId;
-        if (socket.callbacks.hasOwnProperty(callbackKey)){
-            callback = socket.callbacks[callbackKey];
-            delete socket.callbacks[callbackKey];
-            callback(message.data)
+
+    // Client receives data from the server
+    socket.onmessage = function(evt) {
+        // Parse message into JSON  
+        var dataJSON = JSON.parse(evt.data);
+        // Ignore say messages which have no requestId
+        var channel = dataJSON.channel;
+        var requestId = dataJSON.requestId;
+        if (requestId !== null) {
+            // If callback function exists, run it
+            var callback = callbackHash[dataJSON.channel + requestId];
+            if (callback !== undefined) {
+                callback(dataJSON.data);
+                delete callbackHash[requestId];
+            }
         }
     };
     return socket;
-}
+};
 
 var clothosocket = "wss://localhost:8443/websocket";
 
-var testThroughAsync = function (name, message, callback) {
-    asyncTest(name, function () {
-        var socket = getSocket(clothosocket);
-        socket.onopen = function () {
-            socket.send(message, function (data) {
-                callback(data);
-                start();
-            });
-        };
-    });
-};
-
-testThroughAsync("get",
-        new Message("get", "Test Part 1"),
-        function (data) {
-            equal(data.name, "Test Part 1");
-        });
-
-testThroughAsync("query Parts",
-        new Message("query", {"schema":"Part"}),
-        function (data) {
-            equal(data.length, 55);
-        });
-
-testThroughAsync("query BasicParts",
-        new Message("query", {"schema":"BasicPart"}),
-        function (data) {
-            equal(data.length, 54);
-        });
-
-testThroughAsync("query CompositeParts",
-        new Message("query", {"schema":"CompositePart"}),
-        function (data) {
-            equal(data.length, 1); 
-            data = data[0]
-            equal(data.type, "COMPOSITE");
-            //shitty hack to test if string
-            ok(data.composition[0].substring);
-        });
-
-
-
-/*
- * TODO: some kind of promise test
- testThroughAsync("create",
- new Message("create", {"name":"Created Part", "sequence":"GGGGGG"}, "6"),
- function (data) {
- var id = data;
- var message = new Message("get", id, "7");
- send(message, this.socket, function (data) {
- equal(data.sequence, "GGGGGG");
- start();
- });
- });
- */
-
-asyncTest("create", function () {
+asyncTest("get", function() {
     var socket = getSocket(clothosocket);
     socket.onopen = function () {
-        socket.send( new Message("create", {"name":"Created Part", "sequence":"GGGGGG"}), function (data) {
-            var id = data;
-            socket.send(new Message("get", id), function (data) {
-                equal(data.sequence, "GGGGGG");
-                start();
-            });
-        })
-    };
-});
-
-asyncTest("create with schema", function () {
-    var socket = getSocket(clothosocket);
-    socket.onopen = function () {
-        socket.send( new Message("create", {"name":"Created Part 2", "sequence":"CCCC", "schema":"BasicPart"}), function (data) {
-            socket.send(new Message("get", "Created Part 2"), function (data2) {
-                ok(data2.hasOwnProperty("schema"));
-                ok(!(data2.hasOwnProperty("className")));
-                //tear down created data
-                socket.send(new Message("destroy", "Created Part 2"), function(data){
-                    //TODO: ensure actually deleted
-                    start();
-                });
-
-            });
-        })
-    };
-});
-
-testThroughAsync("create LabPerson", 
-        new Message("create", {"schema":"52310822c2e67bf02c9c21f1"}),
-        function (){
-            expect(0);
-        });
-
-asyncTest("set", function (){
-    var socket = getSocket(clothosocket);
-    socket.onopen = function () {
-        socket.send(new Message("get", "Test Part 1"), function (data) {
-            var id = data.id;
-            socket.send(new Message("set", {"id":id, "name":"Set Part"}), function(data){
-                socket.send(new Message("get", id), function (data){
-                    equal(data.name, "Set Part");
-                    socket.send(new Message("set", {"id":id, "name":"Test Part 1"}), function(data){
-                        start();
-                    });
-                });
-            });
+        socket.send("get", "Test Part 1", function (data) {
+                equal(data.name, "Test Part 1");
+            start();
         });
     };
 });
-
-asyncTest("login/logout", function(){
-    var credentials = {"username":"testuser", "password":"password"};
-
-    var socket = getSocket(clothosocket);
-    socket.onopen = function () {
-        socket.send(new Message("login" , credentials), function (data){
-            equal(data, true);
-            socket.send(new Message("submit", "var persistMe = 42"), function (data){
-                socket.send(new Message("submit", "persistMe"), function (data){
-                    equal(data, 42);
-                    socket.send(new Message("logout", ""), function (data) {
-                        equal(data,true);
-                        socket.send(new Message("submit", "persistMe"), function (data) {
-                            notEqual(data, 42, "data retained on logout?");
-                            socket.send(new Message("login", credentials), function(data){
-                                equal(data,true);
-                                socket.send(new Message("submit", "persistMe"), function (data){
-                                    equal(data, 42, "data recovered on login?");
-                                    start();
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    };
-});
-// TODO: add tests for listener dereg
-
-asyncTest("reload models", function(){
-    var socket = getSocket(clothosocket);
-    socket.onopen = function () {
-        socket.send(new Message("get", "ClothoSchema"), function(data){
-            id = data.id;
-            socket.send(new Message("reloadModels", {}), function (response) {
-                socket.send(new Message("get", "ClothoSchema"), function(data){
-                    equal(data.id, id);
-                    start();
-                });
-            });
-        });
-    };
-});
-
-module("Functions and Modules")
-
-asyncTest("changing function", function(){
-    var f1 = {"schema":"Function","language":"JAVASCRIPT", "code":"function(){return 1;};"};
-    var f2 = {schema:"Function",language:"JAVASCRIPT", "code":"function(){return 2;};"};
-
-    var socket = getSocket(clothosocket);
-    socket.onopen = function () {
-        socket.send(new Message("create", f1), function(id){
-            socket.send(new Message("run", {"id":id, "args":[]}), function(result){
-                equal(result, 1);
-                f2.id = id;
-                socket.send(new Message("set", f2), function(data){
-                    socket.send(new Message("run", {id:id, args:[]}), function(result){
-                        equal(result,2);
-                        start();
-                    });
-                });
-            });
-        });
-    };
-});
-
-testThroughAsync("functions with arguments",
-        new Message("submit", "clotho.run('lowercase', ['HEY'])"),
-        function(data){
-            equal(data, "hey");
-        });
-testThroughAsync("module scoping",
-        new Message("run", {id:"moduleTestFunction", args:[1]}),
-        function(data){
-            equal(data, 4);
-        });
-
-testThroughAsync("invoke module method",
-        new Message("run", {id:"testModule", "function":"moduleMethod", args:[]}),
-        function(data){
-            equal(data, 2);
-        });
-
-testThroughAsync("module depends on module",
-        new Message("run", {id:"testModule2","function":"moduleMethod", args:[]}),
-        function(data){
-            equal(data,2);
-        });
-
-testThroughAsync("function depends on function",
-        new Message("run", {id:"moduleTestFunction", args:[1]}),
-        function(data){
-            equal(data,4);
-        });
-
-testThroughAsync("importing external library",
-        new Message("submit", "load(\"yepnope.js\")"),
-        function (data) {
-            equal(data, null);
-        });
-testThroughAsync("use lodash",
-        new Message("run", {id: "useLodash", args:[]}),
-        function(data){
-            deepEqual(data, [2,3,4]);
-        });
-
-module("SynBERC demo issues - all through submit channel")
-
-testThroughAsync("console.log",
-        new Message("submit", "clotho.run('consoleTest',[])"),
-        function(data){
-            equal(data, 'This worked!');
-        });
-
-testThroughAsync("running module functions - revcomp",
-        new Message("submit", "clotho.run('DNA', 'revcomp', ['acgtac'])"),
-        function(data){
-            equal(data, 'gtacgt');
-        });
-
-testThroughAsync("running module functions - ligate",
-        new Message("submit", "clotho.run('PCR', 'ligate', [['aaaaaaaaaaA^CATG_', '^CATG_Tttggttggttgg']])"),
-        function(data){
-            equal(data, "aaaaaaaaaaACATGTttggttggttgg");
-        });
-
-testThroughAsync("loading functions - submit", 
-        new Message("submit", "var DNA = clotho.load('DNA'); DNA.revcomp('acgtacg')"),
-        function (data){
-            equal(data, "cgtacgt");
-        });
-
-testThroughAsync("loading functions - load in function", 
-        new Message("submit", "clotho.run('clothoLoadTest', [])"),
-        function (data){
-            equal(data, 'cgtacgt');
-        });
-
-testThroughAsync("loading functions - global load through function",
-        new Message("submit", "clotho.run('clientSetup', []); DNA.revcomp('ttttacccggg');"),
-        function (data) {
-            equal(data, 'cccgggtaaaa');
-        });
-
-testThroughAsync("functions on objects",
-        new Message("submit", "var myObj = {}; myObj.myFunc = function(str) { return 'hey ' + str; }; myObj.myFunc('Bob')"),
-        function (data) {
-            equal(data, 'hey Bob');
-        });
